d8d2f1ad420d52e531774c911ec3413010f92e55
Sidny8 <telc090311mgtnpna@cbtis217.edu.mx>
2025-11-26 18:57:46 -0600

    Se realizo el problema de potencias

---
+++ EvaluandoPotencia.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   float x,y,z, formula;
+   cin >> x >> y >> z;
+   formula=(7 + pow(2*x + y - z,2*x + y - z))/(2*x+y-z);
+    cout << formula;
+    return 0;
+}
